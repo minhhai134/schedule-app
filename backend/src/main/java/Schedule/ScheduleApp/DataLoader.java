28016be1bf5b51993c22fecf37e46427f2b74f52
Dam Hai <minhhaizdr@gmail.com>
2025-03-13 00:51:32 +0700

    hotfix/ modify config

--- backend/src/main/java/Schedule/ScheduleApp/DataLoader.java
+++ backend/src/main/java/Schedule/ScheduleApp/DataLoader.java
@@ -4,6 +4,9 @@ import Schedule.ScheduleApp.model.*;
 import Schedule.ScheduleApp.model.Resource;
 import Schedule.ScheduleApp.model.Task;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
--- backend/src/main/java/Schedule/ScheduleApp/ScheduleGA.java
+++ backend/src/main/java/Schedule/ScheduleApp/ScheduleGA.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import org.apache.log4j.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.IOException;
 import java.util.HashMap;
 
